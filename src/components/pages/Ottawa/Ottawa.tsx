03d7e744f736aa591d58d2d815ddf7b2ea1a4e62
Karl Chvojka <karl.chvojka@protonmail.com>
2022-08-31 15:48:45 -0400

    IMP: Ottawa

--- src/components/pages/Ottawa/Ottawa.tsx
+++ src/components/pages/Ottawa/Ottawa.tsx
@@ -1,13 +1,66 @@
 // Framework Imports
 import React, { FC } from 'react'
 
+// Component Imports
+import { IconContext } from "react-icons";
+import { WiDaySunny } from "react-icons/wi";
+
+// CSS Imports
+import './Ottawa.less'
+
 /**
  * This the Ottawa component.
  */
 const Ottawa: FC = () => {
   return (
     <section className="ottawaWrap">
-      <h1>Ottawa</h1>
+      <section className="todayWrap">
+        <h1>Today</h1>
+        <section className="todayInner">
+          <section className="todayInnerLeft">
+          <IconContext.Provider value={{ className: "shared-class", size: '20' }}>
+            <WiDaySunny className="wi-fw" />
+          </IconContext.Provider>
+          </section>
+          <section className="todayInnerRight">
+            <section className="innerRightTop">
+              <p>19<span>&#176;</span></p>
+            </section>
+            <section className="innerRightBottom"></section>
+              <h2>Clouds</h2>
+          </section>
+        </section>
+      </section>
+      <section className="forecastWrap">
+        <section className="dayWrap">
+          <h3>Wed</h3>
+          <section className="imgPlaceholder">
+            <WiDaySunny />
+          </section>
+          <p>19<span>&#176;</span></p>
+        </section>
+        <section className="dayWrap">
+          <h3>Thu</h3>
+          <section className="imgPlaceholder">
+            <WiDaySunny />
+          </section>
+          <p>19<span>&#176;</span></p>
+        </section>
+        <section className="dayWrap">
+          <h3>Fri</h3>
+          <section className="imgPlaceholder">
+            <WiDaySunny />
+          </section>
+          <p>19<span>&#176;</span></p>
+        </section>
+        <section className="dayWrap">
+          <h3>Sat</h3>
+          <section className="imgPlaceholder">
+            <WiDaySunny />
+          </section>
+          <p>19<span>&#176;</span></p>
+        </section>
+      </section>
     </section>
   )
 }
