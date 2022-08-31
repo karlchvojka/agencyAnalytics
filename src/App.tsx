d9c1734ba7816fc000e88d97a554a0c64603042b
Karl Chvojka <karl.chvojka@protonmail.com>
2022-08-30 21:59:40 -0400

    REF: App for Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@
 import React, { FC } from 'react'
 
 // Library Imports
+import { Routes, Route, Link } from 'react-router-dom'
+
+// Component Imports
 import Header from 'modules/Header/Header'
 
 // CSS Imports
 import './globalConfig/GlobalStyles.less'
+
 const App: FC = () => {
   return (
-    <section>
-      <Header />
-      <h1>Welcome to React App thats build using Webpack and Babel separately</h1>
-    </section>
+    <Routes>
+      <Route path='/' element={
+        <section>
+          <Header />
+          <h1>Welcome to React App thats build using Webpack and Babel separately</h1>
+        </section>
+      } />
+    </Routes>
   )
 }
 
