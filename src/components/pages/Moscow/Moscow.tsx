c6ebc3a4d3f377b2e5449f690700d01bf0e0b0c9
Karl Chvojka <karl.chvojka@protonmail.com>
2022-08-31 22:15:00 -0400

    IMP: Moscow Component

---
+++ src/components/pages/Moscow/Moscow.tsx
@@ -0,0 +1,102 @@
+// Framework Imports
+import React from 'react'
+
+// Component Imports
+import Day from 'elements/Day/Day'
+import WeatherIco from 'elements/WeatherIcon/WeatherIcon';
+
+// Helper Imports
+import { FetchWeather } from 'src/helpers/FetchWeather.js'
+
+// CSS Imports
+import './Moscow.less'
+
+// Type Declarations
+interface DayItem {
+  dt?: string;
+  temp?: {
+    day?: number
+  };
+  weather?: Array<WeatherItem>;
+}
+
+interface WeatherItem {
+  description?: string;
+}
+
+interface CityState {
+  current: {
+    dt?: number;
+    temp?: number;
+    weather?: Array<WeatherItem>;
+  }; 
+  daily: DayItem[];
+}
+
+interface Props {}
+
+/**
+ * This the Moscow component.
+ */
+class Moscow extends React.Component<Props, CityState> {
+  // State Declarations
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      current: {},
+      daily: []
+    }
+  }
+  
+  // Component process
+  componentDidMount() {
+    FetchWeather('55.7558', '37.6173').then(response => {
+      this.setState({
+        current: response.data.current,
+        daily: response.data.daily
+      });
+    });
+  }
+
+  render() {
+    return (
+      Object.keys(this.state.current).length > 0 ?
+      <section className="moscowWrap">
+        <section className="todayWrap">
+          <h1>Today</h1>
+          <section className="todayInner">
+            <section className="todayInnerLeft">
+            <WeatherIco
+              current={this.state.current.weather[0].description}
+              place={'current'}
+              />
+            </section>
+            <section className="todayInnerRight">
+              <section className="innerRightTop">
+                <p>{Math.round(this.state.current.temp)}<span>&#176;</span></p>
+              </section>
+              <section className="innerRightBottom"></section>
+                <h2>Clouds</h2>
+            </section>
+          </section>
+        </section>
+        <section className="forecastWrap">
+          {
+            this.state.daily.slice(1, 5).map((day: DayItem, index) => {
+              return <Day
+                day={day.dt}
+                key={day.dt}
+                temp={day.temp.day}
+                forecast={day.weather[0].description}
+              />
+            })
+          }
+        </section>
+      </section>
+      :
+      <h2>Loading...</h2>
+    );
+  }
+}
+
+export default Moscow
